0a9dd68f014ad3bda90dd3d0137b4c65ce14d6f9
Nathan Blane <nblane11@gmail.com>
2019-10-10 18:21:44 -0500

    Stats now display the frame time at the very top of the display

--- Source/Musa/Visualization/EngineStatView.cpp
+++ Source/Musa/Visualization/EngineStatView.cpp
@@ -1,6 +1,26 @@
 #include "EngineStatView.hpp"
 #include "Debugging/ProfilerStatistics.hpp"
 #include "Visualization/ScreenTextItem.hpp"
+#include "Engine/FrameData.hpp"
+
+static void MoveMetricPositionDown(Vector2& currentScreenPos)
+{
+	currentScreenPos.y += 20;
+}
+
+static void FormatAndAddTextItem(DynamicArray<ScreenTextItem>& textItems, Vector2& currentScreenPos, const char* metricName, float32 msTime)
+{
+	constexpr uint32 formattedStrLen = 256;
+	tchar formattedString[formattedStrLen];
+	snprintf(formattedString, formattedStrLen,
+		"%s: %5.03fms"
+		//" in %50s: line %4d"
+		, metricName, msTime
+		//, entry.filename, entry.lineCount
+	);
+	textItems.Add(ScreenTextItem{ formattedString, Color32::White(), currentScreenPos, .15f });
+	MoveMetricPositionDown(currentScreenPos);
+}
 
 void EngineStatView::ToggleStats()
 {
@@ -16,22 +36,16 @@ void EngineStatView::PushStatsToView(DynamicArray<ScreenTextItem>& textItems)
 {
 	if (statsVisible)
 	{
-		constexpr uint32 formattedStrLen = 256;
 		Vector2 currentScreenPos(2, 2);
 		const ProfiledFrameMark& frame = GetProfilingStatistics().GetPreviousFrame();
 
+		FormatAndAddTextItem(textItems, currentScreenPos, "Frame Time", Frame::GetTickTimeSeconds() * 1000.f);
+
+		MoveMetricPositionDown(currentScreenPos);
+
 		for (auto& frameMetric : frame.frameMetrics)
 		{
-			tchar formattedString[formattedStrLen];
-			snprintf(formattedString, formattedStrLen,
-				"%s: %5.03fms"
-				//" in %50s: line %4d"
-				, frameMetric.metricName, frameMetric.totalMetricTimeMS
-				//, entry.filename, entry.lineCount
-			);
-			textItems.Add(ScreenTextItem{ formattedString, Color32::White(), currentScreenPos, .15f });
-
-			currentScreenPos.y += 20;
+			FormatAndAddTextItem(textItems, currentScreenPos, frameMetric.metricName, (float32)frameMetric.totalMetricTimeMS);
 		}
 	}
 }
