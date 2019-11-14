9d027a261ca1b289083c45ddf6b4fe63610cd40c
Nathan Blane <nblane11@gmail.com>
2019-11-13 22:47:58 -0600

    Beginning the huge journey of implementing UI in my engine. Currently, I'm just making notes, telling myself what's needed. Next I'll hopefully start on the overall system

--- Source/Musa/UI/GameUIContext.hpp
+++ Source/Musa/UI/GameUIContext.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#include "Containers/DynamicArray.hpp"
+
+struct UIWidget;
+
 class GameUIContext
 {
 public:
+	// Either needs to take in the input handler or whoever "adds" UI must also add it to the input handling
+	GameUIContext() = default;
+
+	// Adds widget tree to the context
+	void AddWidget(UIWidget& widget);
+
+	// Updates all of the UI within the tree. Probably pushes all of the info to the render side of things as well...
+	void Update();
 
 private:
-
+	// Tree of widgets
+	DynamicArray<UIWidget*> widgets;
 };
 
--- Source/Musa/UI/UIWidget.hpp
+++ Source/Musa/UI/UIWidget.hpp
@@ -1,10 +1,22 @@
 #pragma once
 
 #include "Containers/DynamicArray.hpp"
+#include "Input/IInputReceiver.hpp"
 
-struct UIWidget
+// A widget is just a general term for whatever will exist in UI.
+// It could be some sort of interactable widget like a button or it could be something that is only used for layout. 
+struct UIWidget : public IInputReceiver
 {
-	DynamicArray<UIWidget*>
+	// TODO - Not all widgets probably need children. Children needs to be fleshed out more
+	// Widget's children
+	DynamicArray<UIWidget*> children;
 
+	// UI needs some sort of transform for rendering
+
+	// UI needs position information relative to the parent
+	// UI needs dimension information (width and height)
+	// UI needs an anchor point and pivot point
+
+	// UI must implement the IInputReceiver interface to receive all the different input
 };
 
